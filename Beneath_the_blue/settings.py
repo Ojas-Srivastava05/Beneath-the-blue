de276140d56fd638ec89ceb5df071ac71ecbf1f1
Ojas Srivastava <srivastavaojas454@gmail.com>
2025-08-03 15:19:29 +0530

    Refactor settings for environment variables and update dependencies in requirements.txt

--- Beneath_the_blue/settings.py
+++ Beneath_the_blue/settings.py
@@ -1,20 +1,16 @@
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e#!jdlgt%pp+2jxd+l-x66%-)p81#d6iw7f_%_o&5(dtzxwfcn'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-e#!jdlgt%pp+2jxd+l-x66%-)p81#d6iw7f_%_o&5(dtzxwfcn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
